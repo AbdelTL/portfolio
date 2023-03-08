87711f374ef174f5452450e9d1efc6e12833b8a3
tlemcani <abderrahman.tlemcani@etu.unistra.fr>
2023-03-08 23:13:18 +0100

    1 save

---
+++ projet portfolio/app/controllers/SubmitController.php
@@ -0,0 +1,14 @@
+<?php
+include dirname(__FILE__) . '/../servicesImpl/ContactInfo.php';
+
+$contact = new ContactInfo();
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $email = $_POST['email'];
+    $firstname = $_POST['firstname'];
+    $lastname = $_POST['lastname'];
+    $object = $_POST['object'];
+
+    $contact->createPost($email, $firstname, $lastname, $object);
+}
+?>
